1c7905ed69781a96ef486588526920b25b76c507
JasonWandrag <jasonwandrag@hotmail.com>
2022-01-31 10:42:15 +0200

    First commit

---
+++ precache-manifest.b125917fbb6fdc369cb273880c17dd4d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd05997f46fc9975017",
+    "url": "/css/about.c7c842d8.css"
+  },
+  {
+    "revision": "a0d69469bfc55ef584e3",
+    "url": "/css/app.1b7ed15e.css"
+  },
+  {
+    "revision": "d877313978df647bfdd8dad9ba89e064",
+    "url": "/img/Store.d8773139.png"
+  },
+  {
+    "revision": "4468d14609e8a3ab128b0e9dccc679f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcd05997f46fc9975017",
+    "url": "/js/about.8f040215.js"
+  },
+  {
+    "revision": "a0d69469bfc55ef584e3",
+    "url": "/js/app.ad61d004.js"
+  },
+  {
+    "revision": "fab0ff620da390bef6a1",
+    "url": "/js/chunk-vendors.6b3a717d.js"
+  },
+  {
+    "revision": "fb063759328d66d101a5adbb378b1ca9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
